fd7cc7448bbb78c4dcdd82f485fcb75118e3b23b
parveen232 <parveen297m210@gmail.com>
2023-07-19 22:18:35 +0530

    the request & response objects

--- first-expressjs-app/index.js
+++ first-expressjs-app/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 // console.dir(app);
 
-app.use(() => {
+app.use((req, res) => {
     console.log("we got a new request");
+    // console.dir(req)
+    // res.send("Hi, we got your request! This is a response!!")
+    // res.send({ color: 'red'})
+    res.send("<h1>This is a WebPage!</h1>")
 })
 
 app.listen(3000, () => {
