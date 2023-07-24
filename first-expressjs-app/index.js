6351e7ef7765235e83e0f1758f5a6efaed3cc8ae
parveen232 <parveen297m210@gmail.com>
2023-07-24 23:10:47 +0530

    express routing basics

--- first-expressjs-app/index.js
+++ first-expressjs-app/index.js
@@ -2,12 +2,29 @@ const express = require('express');
 const app = express();
 // console.dir(app);
 
-app.use((req, res) => {
-    console.log("we got a new request");
-    // console.dir(req)
-    // res.send("Hi, we got your request! This is a response!!")
-    // res.send({ color: 'red'})
-    res.send("<h1>This is a WebPage!</h1>")
+// app.use((req, res) => {
+//     console.log("we got a new request");
+//     // console.dir(req)
+//     // res.send("Hi, we got your request! This is a response!!")
+//     // res.send({ color: 'red'})
+//     res.send("<h1>This is a WebPage!</h1>")
+// })
+
+app.get('/fruits', (req, res) => {
+    // console.log("request for fruits")
+    res.send("<h1>Fruits Page</h1>")
+})
+
+app.post('/create', (req, res) => {
+    res.send("post request to /create, this is different than a get request!")
+})
+
+app.get('/languages', (req, res) => {
+    res.send("<h1>Languages Page</h1>")
+})
+
+app.get('*', (req, res) => {
+    res.send("<h1>I don't know that path</h1>")
 })
 
 app.listen(3000, () => {
