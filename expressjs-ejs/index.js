7adc511660c86228e13e587b041661110d195156
parveen232 <parveen297m210@gmail.com>
2023-08-03 22:21:04 +0530

    ejs interpolation syntax & passing data to templates

--- expressjs-ejs/index.js
+++ expressjs-ejs/index.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.render('home.ejs')
 })
 
+app.get('/random', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { randNum: num })
+})
+
 app.listen(3000, () => {
     console.log('listening on port 3000');
 })
