d731af0bcdb5c8828b6da324b00712767afead34
parveen kumar <parveen297m210@gmail.com>
2023-08-28 13:07:11 +0530

    serving static assets

--- expressjs-ejs/index.js
+++ expressjs-ejs/index.js
@@ -1,31 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.render('home.ejs')
-})
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
-app.get('/fruits', (req, res) => {
-    const fruits = [
-        'apple', 'banana', 'orange', 'grape', 'mango'
-    ]
-    res.render('fruits', { fruits })
-})
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
 
-app.get('/r/:subreddit', (req, res) => {
-    const { subreddit } = req.params;
-    res.render('subreddit', { subreddit })
-})
+app.get("/fruits", (req, res) => {
+  const fruits = ["apple", "banana", "orange", "grape", "mango"];
+  res.render("fruits", { fruits });
+});
 
-app.get('/random', (req, res) => {
-    const num = Math.floor(Math.random() * 10) + 1;
-    res.render('random', { randNum: num })
-})
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.render("subreddit", { subreddit });
+});
+
+app.get("/random", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render("random", { randNum: num });
+});
 
 app.listen(3000, () => {
-    console.log('listening on port 3000');
-})
+  console.log("listening on port 3000");
+});
