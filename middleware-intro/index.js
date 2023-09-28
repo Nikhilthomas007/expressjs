c4574cd73377868079c35d6e01c0c56131b70a75
parveen kumar <parveen297m210@gmail.com>
2023-09-28 19:01:34 +0530

    Defining our own middleware

--- middleware-intro/index.js
+++ middleware-intro/index.js
@@ -4,11 +4,30 @@ const morgan = require('morgan');
 
 app.use(morgan('tiny'))
 
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
+})
+
+// app.use((req, res, next) => {
+//     console.log("This is my first middleware!")
+//     // next();
+//     return next();
+//     console.log("This is my first middleware - after calling next()")
+// })
+// app.use((req, res, next) => {
+//     console.log("This is my second middleware!")
+//     next();
+// })
+
 app.get('/', (req, res) => {
+    console.log(`Request Date: ${req.requestTime}`)
     res.send("<h1>Home Page</h1>")
 })
 
 app.get('/fruits', (req, res) => {
+    console.log(`Request Date: ${req.requestTime}`)
     res.send("<h1>Fruits List</h1>")
 })
 
